f131786a5dd707692d74fa5e3675ffcb0a7c8f0f
Gastunchy <97688245+Gastunchy@users.noreply.github.com>
2024-02-28 17:15:57 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,30 +7,43 @@ const cors = require('cors')
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("../swagger-openapi.json");
 
-
 const app = express()
 
+// Configurar middleware de CORS para permitir solo la dirección web especificada
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (origin === "http://front.34.73.198.248.nip.io") {
+            callback(null, true);
+        } else {
+            callback(new Error("Acceso prohibido"));
+        }
+    }
+};
 
-app.use(cors())
+app.use(cors(corsOptions));
+
 app.use(express.json())
 app.use('/api', indexRouter)
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 const NotFounded = (req, res, next) => {
-    next(createError(404), "No se encontró la direccion que busca")
+    next(createError(404), "No se encontró la dirección que busca")
 }
 const errorHandler = (err, req, res, next) => {
-    res.status(err.status || 500)
-    res.send({
+    res.status(err.status || 500);
+    res.json({
         status: err.status || 500,
-        message: err.message
-    })
+        error: {
+            message: 'Acceso prohibido'
+        },
+        message: "Error interno del servidor"
+    });
 }
 
 app.use(NotFounded)
 app.use(errorHandler)
 
 app.listen(3000, () => {
-    console.log("listenen on port 3000");
+    console.log("Escuchando en el puerto 3000");
 })
